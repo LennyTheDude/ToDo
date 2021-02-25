eadaef1f1cbd019f2ce45ef8b72818d33dfec4d6
Lev Antipenko <lennyfromrussia@gmail.com>
2021-02-25 16:00:02 +0300

    Better logic in PUT route

--- routes/tasks/task.put.js
+++ routes/tasks/task.put.js
@@ -3,27 +3,23 @@ const router = express.Router()
 const Task = require('../../models').Task
 
 router.put('/task/', async (req, res) => {
+    
     const updateTask = req.body;
-    console.log(updateTask);
+
     if (!updateTask.taskId) {
         res.send("not found");
     }
 
-    if (updateTask.name) {
-        const editTask = await Task.update(
-            { taskName: updateTask.name}, 
-            { where: {id: updateTask.taskId}}
-        )
-    } else {
-        const editTask = await Task.update(
-            { isDone: updateTask.isDone}, 
-            { where: {id: updateTask.taskId}}
-        )
-    }
+    const newTask = {};
+    if (updateTask.hasOwnProperty('name')) { newTask.taskName = updateTask.name }
+    if (updateTask.hasOwnProperty('isDone')) { newTask.isDone = updateTask.isDone }
 
-    const task = await Task.findOne({where: {id: updateTask.taskId}});    
-    // console.log(task);
-    res.status(200).send(task)
+    const editedTask = await Task.update(
+        newTask, 
+        { where: {id: updateTask.taskId}, returning: true, plain: true}
+    );
+    
+    res.status(200).send(editedTask[1])
 })
 
 module.exports = router
